3e465161ed995c6bd88ebf123abbb22af63a4b94
Ragil <mochragilwcaksono999@gmail.com>
2021-09-15 14:20:32 +0700

    Create Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,34 @@
+<?php   
+    $Nama = "<b>Moch Ragil Wicaksono</b>";
+    $Kelas = "<b>MI20B</b>";
+    $alamat = "<b>leuwidahu - Tajur indah</b>";
+    $email  = "<b>mochragilwicaksono999@gmail.com</b>";
+    $No = "<b>0812312425111</b>";
+?>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <div style ="text-align :center">
+        
+        <?php 
+        echo "<br>";
+        echo "<b>MY BIODATA</b>";
+        echo "<br> <br>";
+        echo "<hr>";
+        echo "Nama Saya : " .$Nama;
+        echo "<br>";
+        echo "Kelas : " .$Kelas;
+        echo "<br>";
+        echo "Alamat : " .$alamat;
+        echo "<br>";
+        echo "Email : " .$email;
+        echo "<br>";
+        echo "No Hp : " .$No;
+        ?>        
+    
+        </div>
+       
+    </body>
+</html>

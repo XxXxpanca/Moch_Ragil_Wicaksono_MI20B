6cd76aaf24a1d9bf5ceebecae8b606ea1da31d0b
Ragil <mochragilwcaksono999@gmail.com>
2021-09-22 10:52:11 +0700

    Create Hi world.php

---
+++ Hi world.php
@@ -0,0 +1,22 @@
+<?php 
+$nama = "Mikey";
+$nama1= "<b>Revenger</b>";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+     <title>First Week - Moch Ragil Wicaksono</title>
+</head>
+
+<body>
+    <?php
+    echo "Haloo lord";
+    echo "<br>";
+    echo $nama;
+    echo "<br>";
+    echo "Welcome in Tokyo " .$nama1;
+    ?>
+</body>
+
+</html>

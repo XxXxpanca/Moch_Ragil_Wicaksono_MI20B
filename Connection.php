5ddd5fb14f0ca18a4efd7f009b3229085e595281
Ragil <mochragilwcaksono999@gmail.com>
2021-09-29 14:09:48 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -6,22 +6,9 @@
 
     //BUAT KONEKSI NYA
     $con = mysqli_connect('localhost' , $username , $password , $database);
-    $result = mysqli_query($con , "SELECT * FROM tbl_surat");
+    $result = mysqli_query($con , "SELECT * FROM tbl_surat WHERE id='2'");
 
-    $data_arr = [];
-
-    while ($data = mysqli_fetch_assoc($result)) {
-        $data_arr[] = $data;
-    }
-
-    echo json_encode($data_arr);
-    die;
-
-    if($con){
-        echo 'Konek cuk';
-    } else {
-        echo 'Failed Connect';
-    }
-
+    $data = mysqli_fetch_assoc($result);
+    
 
 ?>
